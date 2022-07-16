0e8887d9dbe84a3e9729d619562894642b5feabf
Pietro Violo <pietro.violo@umontreal.ca>
2022-07-16 15:35:00 -0400

    Final product

--- venezuela_exodus.R
+++ venezuela_exodus.R
@@ -13,18 +13,6 @@ remotes::install_github("davidsjoberg/ggsankey")
 library(ggsankey)
 library(tidyverse)
 
-
-df <- mtcars %>%
-  make_long(cyl, vs, am, gear, carb) 
-
-ggplot(df, aes(x = x, 
-               next_x = next_x, 
-               node = node, 
-               next_node = next_node,
-               fill = factor(node))) +
-  geom_sankey() +
-  theme_sankey(base_size = 16)
-
 data <- data.frame(next_node =  c(1800000,
                                   1300000,
                                   513900,
@@ -48,45 +36,39 @@ data <- data.frame(next_node =  c(1800000,
                      "Argentina",
                      "Panama",
                      "Dominican Republic",
-                     "Other"))) %>% 
- 
-  library(tidyverse)
-library(networkD3)
+                     "Other")))
 
-data <- tibble::tribble(
-  ~Name, ~Annual.Amount,  ~Category,
-  "Moira's Earnings",         50000L,   "Income",
-  "Johnny's Earnings",        300000L,   "Income",
-  "Living Expenses",        140000L, "Expenses",
-  "Spent Savings",         25238L, "Expenses",
-  "Liabilities",         44280L, "Expenses",
-  "Planned Savings",         23000L, "Expenses",
-  "Taxes",         98482L, "Expenses",
-  "Insurance",         13000L, "Expenses"
-)
+data$next_node/100
 
-Nodes = tibble(
-  Name = c(data$Name, "Budget") %>% unique()
-) %>% as.data.frame()
+# GG sankey way of handling things is irrational, so we have to make a dataframe
+# to correspond its required format
+
+# And because it will order them alphabetically, we will have to use
+# abc as the variable names while remembering the order of the true 
+# countries when we will edit in Photoshop
 
 
-df = data %>% filter(Category=="Income") %>%
-  select(-Category) %>%
-  rename(Source = Name) %>%
-  mutate(Target = "Budget") %>%
-  bind_rows(
-    data %>%
-      filter(Category=="Expenses") %>%
-      select(-Category) %>%
-      rename(Target = Name) %>%
-      mutate(Source = "Budget")
-  ) %>% mutate(
-    IDSource = match(Source, Nodes$Name)-1,
-    IDTarget = match(Target, Nodes$Name)-1
-  ) %>% as.data.frame()
+Destination = c(rep("a",18000),rep("b", 13000),rep("c", 5139),
+  rep("d", 4652), rep("e", 4481), rep("f",4182),
+  rep("g", 3450), rep("h", 1703), rep("i", 1216),
+  rep("j", 1153), rep("k",4024))
 
-sankeyNetwork(Links = df, Nodes = Nodes,
-              Source = "IDSource", Target = "IDTarget",
-              Value = "Annual.Amount", NodeID = "Name",
-              sinksRight=FALSE, fontSize = 16)
+data <- data.frame(Origin = "", Destination = Destination,
+                   Origin2 = "Venezuela") %>% 
+  ggsankey::make_long(Origin, Destination, Origin2)
 
+# Save graph
+
+png("venezuela_exodus.png", res = 300, width = 2000, height = 1200)
+
+ggplot(data, aes(x = x, next_x = next_x, 
+                node = node, next_node = next_node, 
+                fill = factor(node), label = node)) +
+  geom_sankey(flow.alpha = 3, node.color = "gray30", width = 0.01, smooth = 6) +
+  geom_sankey_label(size = 3, color = "white", fill = "gray40") +
+  theme_sankey(1) +
+  labs(x = NULL) +
+  theme(legend.position = "none",
+        plot.title = element_text(hjust = .5))
+
+dev.off()
